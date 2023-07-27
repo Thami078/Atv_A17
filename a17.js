c97414453615ef0026266df8c611fc8bb49fcb65
Potato0708 <tca7alvs@gmail.com>
2023-07-27 18:41:57 -0300

    coment1

---
+++ a17.js
@@ -0,0 +1,7 @@
+var receberNome = prompt ("Digite seu nome completo")
+var quantasLetras = receberNome.length;
+
+document.write("<p>" + receberNome + "</p>")
+
+document.write ("<p2>" + quantasLetras + "</p2>")
+
